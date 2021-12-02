1da94a3e7255fc6328a224ac0db13a2cfe524bf6
Christian Rogobete <c.rogobete@soneso.com>
2021-12-02 14:50:01 +0100

    add contract logic to create proposal contract

--- smart-contracts/src/txFunctionCreateProposal.js
+++ smart-contracts/src/txFunctionCreateProposal.js
@@ -4,7 +4,8 @@ const {
     Networks,
     BASE_FEE,
     Keypair,
-    Operation
+    Operation,
+    Asset
 } = require('stellar-sdk')
 
 const server = new Server(HORIZON_URL)
@@ -16,21 +17,51 @@ module.exports = async (body) => {
         proposalSecret,
         tallySecret,
         tallyContractSigners,
+        proposalContractSigners,
         IpfsProposalAddr
     } = body
 
     console.log("source: ", source);
 
     const proposalKeypair = Keypair.fromSecret(proposalSecret);
-    const tallyKeypair = Keypair.fromSecret(tallySecret);
     const proposalAccountId = proposalKeypair.publicKey();
-    const tallyAccountId = tallyKeypair.publicKey();
 
-    console.log("proposalAccountId: ", proposalAccountId);
-    console.log("tallyAccountId: ", tallyAccountId);
+    const tallyKeypair = Keypair.fromSecret(tallySecret);
+    const tallyAccountId = tallyKeypair.publicKey();
+    const tallyTurretSigners = tallyContractSigners.split(',').map(function(item) {
+        return item.trim();
+    });;
+
+    if (tallyTurretSigners.length < 3) {
+        throw {
+            message: 'Not enough tally turret contract signers.'
+        }
+    }
+
+    const proposalTurretSigners = proposalContractSigners.split(',').map(function(item) {
+        return item.trim();
+    });;
+
+    if (proposalTurretSigners.length < 3) {
+        throw {
+            message: 'Not enough proposal turret contract signers.'
+        }
+    }
+
+    if (tallyTurretSigners.length != proposalTurretSigners.length) {
+        throw {
+            message: 'Number of proposal turret contract signers and tally turret contract signers must be the same.'
+        }
+    }
+
+    // test data
+    const nrOfOptions = 4;
+
+    // end test data
+
+    const nrOfSigners = proposalTurretSigners.length;
 
     const fee = await getFee();
-    console.log("fee: ", fee);
 
     const sourceAccount = await server.loadAccount(source);
 
@@ -48,6 +79,80 @@ module.exports = async (body) => {
         destination: tallyAccountId,
         startingBalance: '1'
     }));
+
+    for (const tallyTurretSigner of tallyTurretSigners) {
+        transaction.addOperation(Operation.setOptions({
+            source: proposalAccountId,
+            signer: {
+                ed25519PublicKey: tallyTurretSigner,
+                weight: 1
+            }
+        }));
+
+        transaction.addOperation(Operation.setOptions({
+            source: tallyAccountId,
+            signer: {
+                ed25519PublicKey: tallyTurretSigner,
+                weight: 1
+            }
+        }));
+    }
+
+    for (const proposalTurretSigner of proposalTurretSigners) {
+        transaction.addOperation(Operation.setOptions({
+            source: proposalAccountId,
+            signer: {
+                ed25519PublicKey: proposalTurretSigner,
+                weight: 1
+            }
+        }));
+
+        transaction.addOperation(Operation.setOptions({
+            source: tallyAccountId,
+            signer: {
+                ed25519PublicKey: proposalTurretSigner,
+                weight: 1
+            }
+        }));
+    }
+
+    transaction.addOperation(Operation.setOptions({
+        source: proposalAccountId,
+        masterWeight: 0,
+        lowThreshold: nrOfSigners,
+        medThreshold: nrOfSigners,
+        highThreshold: nrOfSigners
+    }));
+
+    transaction.addOperation(Operation.setOptions({
+        source: tallyAccountId,
+        masterWeight: 0,
+        lowThreshold: nrOfSigners,
+        medThreshold: nrOfSigners,
+        highThreshold: nrOfSigners
+    }));
+
+    transaction.addOperation(Operation.manageData({
+        source: proposalAccountId,
+        name: "status",
+        value: "active"
+    }));
+
+    transaction.addOperation(Operation.manageData({
+        source: proposalAccountId,
+        name: "createTime",
+        value: "" + Math.floor(Date.now() / 1000)
+    }));
+
+    const optionAssets = new Array(nrOfOptions);
+
+    for (let i = 0; i < optionAssets.length; i++) {
+        transaction.addOperation(Operation.changeTrust({
+            source: tallyAccountId,
+            asset: new Asset('OPTION' + i, proposalAccountId)
+        }));
+    }
+
 
     return transaction.setTimeout(0).build().toXDR('base64');
 
